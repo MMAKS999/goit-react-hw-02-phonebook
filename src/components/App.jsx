e1d61cfb3a209b712be5758e054f2a170fd2e676
MAX999 <max999b@gmail.com>
2023-06-03 20:47:24 +0300

    react-hw2-Chack
    написаний функціонал без оформлення стилями

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,70 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+
+import { Filter } from "./Filter";
+import { PhonebookForm } from "./PhonebookForm";
+import { ContactList } from './contactList';
+import Swal from 'sweetalert2'
+
+const { Component } = require("react");
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: ''
+  };
+
+  addContact = (dataContact) => {
+    // console.log(dataContact)
+    if (this.state.contacts.some(contact => contact.name === dataContact.name)) {
+      Swal.fire(`${dataContact.name} is already in contacts.`)
+    } else {
+      this.setState(prevState => ({
+        contacts:[dataContact, ...prevState.contacts]
+      }))
+      // console.log(this.state)  
+    }
+  };
+
+  changeFilter = (ev) => {
+    this.setState({filter: ev.currentTarget.value})
+  };
+
+  // Видалення контакту 
+  onDelateContact = (id) => {
+    // console.log(id);
+    const { contacts } = this.state;
+    const updatedContacts = contacts.filter(contact => contact.id !== id);
+    this.setState({ contacts: updatedContacts });
+  };
+  
+  // метод фільтрації контактів
+  getVisibleContacts = () => {
+    const { contacts, filter } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    // console.log(filter)
+    return contacts.filter(contact => contact.name.toLowerCase().includes(normalizedFilter));
+  };
+  
+  render() {
+    const {filter} = this.state;
+
+    const visibleContacts = this.getVisibleContacts();
+
+     return (
+       <div>
+         <h1>Phonebook</h1>
+         <PhonebookForm
+           onSubmit = {this.addContact}
+         />
+         <h2>Contacts</h2>
+          <Filter
+           value= {filter}
+           onChange={this.changeFilter}
+         />
+         <ContactList
+           dataContacts={visibleContacts}
+            onDelateContact = {this.onDelateContact}
+         />
+      </div>
+    );
+  };
 };
---
+++ src/components/PhonebookForm.jsx
@@ -0,0 +1,84 @@
+import { nanoid } from 'nanoid'
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const { Component } = require("react");
+
+
+const Form = styled.form`
+  border: 1px solid #ccc;
+  padding: 10px;
+  display: flex;
+  width: 500px;
+ `;
+
+export class PhonebookForm extends Component{
+  state = {
+    name: '',
+    number: '',
+    id: ''
+  };
+// генерація id
+  // modelIid = nanoid()
+ 
+
+  handleChange = (ev) => {
+    // console.log(ev.currentTarget.value)
+    const { name, value } = ev.currentTarget;
+    this.setState({ id: nanoid() })
+    this.setState({ [name]: value });
+  };
+ 
+  // считування інформації з інпуту і 
+  handleSubmit = ev => {
+    ev.preventDefault();
+
+    // console.log(this.state)
+    // console.log(this.modelIid)
+    // Перекидаємо в Арр
+    this.props.onSubmit(this.state)
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({
+      id: '',
+      name: '',
+      number: ''
+    })
+  };
+
+  render() {
+    return (
+      <Form onSubmit={this.handleSubmit}>
+        <label>
+          Name<input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            value={this.state.name}
+            onChange={this.handleChange}
+           />
+        </label>
+        <label >
+          Number<input
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[\-.\s]?\(?\d{1,3}?\)?[\-.\s]?\d{1,4}[\-.\s]?\d{1,4}[\-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            value={this.state.number}
+            onChange={this.handleChange}
+          />
+        </label>
+        <button type="submit">Add contact</button>
+        
+      </Form>
+    );
+  };
+  
+}
+
+
